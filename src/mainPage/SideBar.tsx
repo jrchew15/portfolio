64540d3fa94e9ade5fcdbb3e9f5cd97ba3bb6f3c
Jason Chew <jrchew15@gmail.com>
2026-04-03 11:35:45 -0700

    lucide and uncolored side bar

--- src/mainPage/SideBar.tsx
+++ src/mainPage/SideBar.tsx
@@ -1,27 +1,139 @@
 import clsx from "clsx";
 import { Container } from "../common/Container";
-import type { Color } from "../theme/colors";
+import { useState } from "react";
+import {
+  LucideArrowLeftToLine,
+  LucideArrowRightToLine,
+  LucideScrollText,
+} from "lucide-react";
 
-const SideBarButton = ({ title, color }: { title: string; color: Color }) => {
-  const classNames = clsx({
-    ["sidebar-button"]: true,
-    ["bg-rosekiss-500"]: color === "rosekiss",
-    ["hover:bg-rosekiss-600"]: color === "rosekiss",
-    ["bg-mutedteal-500"]: color === "mutedteal",
-    ["hover:bg-mutedteal-600"]: color === "mutedteal",
-    ["bg-steelblue-500"]: color === "steelblue",
-    ["hover:bg-steelblue-600"]: color === "steelblue",
+const SideBarButton = ({
+  title,
+  expanded,
+  groupHovered,
+}: {
+  title: string;
+  expanded?: Boolean;
+  groupHovered?: Boolean;
+}) => {
+  const buttonClassNames = clsx(
+    "sidebar-button",
+    "flex",
+    "items-center",
+    " hover:bg-jetblack-450",
+    {
+      ["w-12"]: !expanded,
+      ["w-60"]: expanded,
+    },
+  );
+
+  const titleClassNames = clsx(
+    "sidebar-button-content",
+    "w-60",
+    "text-center",
+    {
+      "opacity-0": !expanded && !groupHovered,
+      "delay-200": groupHovered,
+    },
+  );
+
+  const iconClassNames = clsx("sidebar-button-content", "relative", {
+    "w-0": expanded || groupHovered,
+    "w-[24px]": !expanded && !groupHovered,
+    "opacity-0": expanded || groupHovered,
+    "ml-1.5": true,
   });
 
-  return <button className={classNames}>{title}</button>;
+  const buttonContent = (
+    <>
+      <div className={iconClassNames}>
+        <LucideScrollText className={iconClassNames} color="#fafaff" />
+      </div>
+      <span className={titleClassNames}>{title}</span>
+    </>
+  );
+
+  return <button className={buttonClassNames}>{buttonContent}</button>;
 };
 
 export const SideBarButtons = () => {
+  const [expandAllButtons, setExpandAllButtons] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const closeStyle = clsx("absolute", "transition-opacity", "duration-300", {
+    "opacity-0": !expandAllButtons,
+    "delay-200": expandAllButtons,
+    "z-1": expandAllButtons,
+  });
+  const openStyle = clsx("absolute", "transition-opacity", "duration-300", {
+    "opacity-0": expandAllButtons,
+    "delay-200": !expandAllButtons,
+    "z-1": !expandAllButtons,
+  });
+
+  const sidebarClasses = clsx(
+    "group",
+    "hover:border-r-ghostwhite-500",
+    "border-r-jetblack-400",
+    "duration:2500",
+    "h-dvh",
+    "border-r-2",
+    "py-2",
+    "transition-all",
+    "ease-in",
+    {
+      ["w-12"]: !isHovered && !expandAllButtons,
+      ["w-60"]: isHovered || expandAllButtons,
+      ["delay-300"]: !isHovered,
+    },
+  );
+
   return (
-    <Container className="flex max-w-fit flex-col items-start justify-around space-y-2">
-      <SideBarButton title="Resume" color="steelblue" />
-      <SideBarButton title="About Me" color="rosekiss" />
-      <SideBarButton title="Carcassone" color="mutedteal" />
-    </Container>
+    <>
+      <Container
+        id="sidebar"
+        className={sidebarClasses}
+        onMouseEnter={() => {
+          setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          setIsHovered(false);
+        }}
+      >
+        <Container className="flex w-fit flex-col items-start justify-between space-y-2">
+          <SideBarButton
+            title="Resume"
+            expanded={expandAllButtons}
+            groupHovered={isHovered}
+          />
+          <SideBarButton
+            title="About Me"
+            expanded={expandAllButtons}
+            groupHovered={isHovered}
+          />
+          <SideBarButton
+            title="Carcassone"
+            expanded={expandAllButtons}
+            groupHovered={isHovered}
+          />
+        </Container>
+      </Container>
+      <div className="group-hover:bg-jetblack-450 hover:bg-jetblack-400 transition-bg ml-2 flex max-h-11 min-h-11 max-w-11 min-w-11 cursor-pointer items-center justify-center rounded-full p-0 duration-200">
+        <LucideArrowLeftToLine
+          color="#fafaff"
+          className={closeStyle}
+          onClick={() => {
+            setExpandAllButtons(false);
+          }}
+        />
+        <LucideArrowRightToLine
+          color="#fafaff"
+          className={openStyle}
+          onClick={() => {
+            setExpandAllButtons(true);
+          }}
+        />
+      </div>
+    </>
   );
 };
